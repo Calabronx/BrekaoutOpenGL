595b2a58a9a82659759f0a7eaf1a23851c9394dd
Calabronx <sebicala@hotmail.com>
2023-09-06 20:55:28 -0300

    game object and game level classes,textures for bricks and background image.Actually not compiling for linking errors to solve tomorrow

--- Game.cpp
+++ Game.cpp
@@ -23,7 +23,25 @@ void Game::Init()
 	ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
 	ResourceManager::GetShader("sprite").Use().SetMatrix4("projection", projection);
 	Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
+	//load textures
+	ResourceManager::LoadTexture("textures/background.jpg", false, "background");
 	ResourceManager::LoadTexture("textures/awesomeface.png", true, "face");
+	ResourceManager::LoadTexture("textures/block.png", true, "block");
+	ResourceManager::LoadTexture("textures/block_solid.png", true, "block_solid");
+	// load levels
+	GameLevel one;
+	GameLevel two;
+	GameLevel three;
+	GameLevel four;
+	one.Load("levels/one.lvl", this->Width, this->Height / 2);
+	two.Load("levels/two.lvl", this->Width, this->Height / 2);
+	three.Load("levels/three.lvl", this->Width, this->Height / 2);
+	four.Load("levels/four.lvl", this->Width, this->Height / 2);
+	this->Levels.push_back(one);
+	this->Levels.push_back(two);
+	this->Levels.push_back(three);
+	this->Levels.push_back(four);
+	this->Level = 0;
 
 }
 
@@ -37,5 +55,15 @@ void Game::Update(float dt)
 
 void Game::Render()
 {
-	Renderer->DrawSprite(ResourceManager::GetTexture("face"), glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+	/*Renderer->DrawSprite(ResourceManager::GetTexture("face")
+		, glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f),
+		45.0f, glm::vec3(0.0f, 1.0f, 0.0f));*/
+	if (this->State == GAME_ACTIVE)
+	{
+		//draw background
+		Renderer->DrawSprite(ResourceManager::GetTexture("background"),
+			glm::vec2(0.0f, 0.0f), glm::vec2(this->Width, this->Height), 0.0f);
+		//draw level
+		this->Levels[this->Level].Draw(*Renderer);
+	}
 }
--- Game.h
+++ Game.h
@@ -1,8 +1,11 @@
 #ifndef  GAME_H
 #define  GAME_H
+#include <vector>
 
-//#include "glad/glad.h"
-//#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "game_level.h"
 
 enum GameState {
 	GAME_ACTIVE,
@@ -12,9 +15,11 @@ enum GameState {
 
 class Game {
 public:	
-	GameState	 State;
-	bool	     Keys[1024];
-	unsigned int Width, Height;
+	GameState			   State;
+	bool				   Keys[1024];
+	std::vector<GameLevel> Levels;
+	unsigned int		   Level;
+	unsigned int		   Width, Height;
 	Game(unsigned int width, unsigned int height);
 	~Game();
 	void Init();
